836da46ddeccc90e8fb434f9a9da5bd3c5eab78c
sm riad <riad@abatacommerce.com>
2022-04-25 10:04:28 +0800

    identity updated

---
+++ EntityFramework App/Identity/ApplicationUserManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EntityFramework_App.Identity
+{
+    public class ApplicationUserManager: UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store)
+        {
+
+        }
+    }
+}
